6951322d673c59a6aaec37e4c58dd783e84ef6d3
Matt van Voorst <mattvanvoorst@contentful.com>
2022-11-24 16:53:48 +0100

    feat: [PLATO-0000] ChakraUI - This PR introduces ChakraUI

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -1,34 +1,19 @@
-/** @jsxImportSource @emotion/react */
-import tokens from '@contentful/f36-tokens';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { Box, Flex } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
 import Logo from '@icons/bonelli.svg';
 import { LanguageSelector } from '@src/components/features/LanguageSelector';
 
-const Nav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  background-color: ${tokens.colorWhite};
-  padding: ${tokens.spacingM} ${tokens.spacingL};
-`;
-
 export const Header = () => {
   const { t } = useTranslation();
 
   return (
-    <Nav>
+    <Flex as="nav" justifyContent="space-between" bg="colorWhite" py={10} px={10}>
       <Link href="/" title={t('common.homepage')}>
-        <Logo
-          css={css`
-            margin-top: 6px;
-          `}
-          title={t('common.logoImageAltText')}
-        />
+        <Box as={Logo} mt={2} title={t('common.logoImageAltText')} />
       </Link>
       <LanguageSelector />
-    </Nav>
+    </Flex>
   );
 };
