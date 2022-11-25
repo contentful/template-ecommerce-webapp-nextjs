77bb80caa97069a88e05eb91487e18e01975a2f7
Nkoyo Ating <atingenkay@gmail.com>
2022-11-25 09:39:29 +0100

    PLATO-272: refactor footer styles

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -9,39 +9,31 @@ const Container = styled.footer`
   margin-bottom: ${({ theme }) => theme.f36.spacingXl};
 `;
 
-const FooterContent = styled.div`
-  margin-left: ${({ theme }) => theme.f36.spacingXl};
-
-  > p {
-    font-size: ${({ theme }) => theme.f36.fontSizeM};
-  }
-`;
-
 export const Footer = () => {
   const { t } = useTranslation();
   const theme = useTheme();
 
   return (
     <Container>
-      <Box>
-        <hr />
-        <FooterContent>
-          <Text my={8}>{t('common.aboutUs')}</Text>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur. Placerat ut purus nam ac morbi erat ut. Laoreet
-            duis augue vestibulum sed egestas tincidunt.
-          </Text>
-          <Text mb={8}>
-            Aliquet vitae consequat quam ut. Integer turpis sit porttitor rhoncus id quisque sed
-            ullamcorper dolor. Commodo.
-          </Text>
-          <Text>
-            {t('common.poweredBy')}{' '}
-            <Link href="https://contentful.com" isExternal color={theme.f36.blue500}>
-              Contentful
-            </Link>
-          </Text>
-        </FooterContent>
+      <hr />
+      <Box ml={8}>
+        <Text my={8} fontSize="sm">
+          {t('common.aboutUs')}
+        </Text>
+        <Text fontSize="sm">
+          Lorem ipsum dolor sit amet consectetur. Placerat ut purus nam ac morbi erat ut. Laoreet
+          duis augue vestibulum sed egestas tincidunt.
+        </Text>
+        <Text mb={8} fontSize="sm">
+          Aliquet vitae consequat quam ut. Integer turpis sit porttitor rhoncus id quisque sed
+          ullamcorper dolor. Commodo.
+        </Text>
+        <Text fontSize="sm">
+          {t('common.poweredBy')}{' '}
+          <Link href="https://contentful.com" isExternal color={theme.f36.blue500}>
+            Contentful
+          </Link>
+        </Text>
       </Box>
     </Container>
   );
