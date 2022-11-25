d0ab072205e2f49784de4112e0f0d0c598e9608a
Matt van Voorst <mattvanvoorst@contentful.com>
2022-11-25 15:58:26 +0100

    feat: [PLATO-0000] font styles - Added an image loader

--- src/components/features/contentful/CtfImage.tsx
+++ src/components/features/contentful/CtfImage.tsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
+import { useState } from 'react';
 
 import { ImageFieldsFragment } from '@src/lib/__generated/sdk';
 
@@ -7,23 +9,31 @@ interface ImageProps extends ImageFieldsFragment {
 }
 
 export const CtfImage = ({ url, width, height, title, imageProps }: ImageProps) => {
+  const [loaded, setLoaded] = useState(false);
+
   if (!url || !width || !height) return null;
 
   const blurURL = new URL(url);
   blurURL.searchParams.set('w', '10');
 
   return (
-    <NextImage
-      src={url}
-      width={width}
-      height={height}
-      alt={title || ''}
-      sizes="(max-width: 768px) 100vw,
+    <motion.div
+      animate={{
+        opacity: loaded ? 1 : 0,
+      }}>
+      <NextImage
+        src={url}
+        width={width}
+        height={height}
+        alt={title || ''}
+        onLoad={() => setLoaded(true)}
+        sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-      placeholder="blur"
-      blurDataURL={blurURL.toString()}
-      {...imageProps}
-    />
+        placeholder="blur"
+        blurDataURL={blurURL.toString()}
+        {...imageProps}
+      />
+    </motion.div>
   );
 };
