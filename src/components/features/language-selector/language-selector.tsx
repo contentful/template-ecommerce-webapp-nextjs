a86bf4dd4ce7e323b07dd9fa9a6cfde29a6eaeb9
Nkoyo Ating <atingenkay@gmail.com>
2022-12-01 14:15:54 +0100

    PLAT0-322: hide select for mobile view

--- src/components/features/language-selector/language-selector.tsx
+++ src/components/features/language-selector/language-selector.tsx
@@ -6,6 +6,7 @@ import {
   MenuItemOption,
   Button,
   MenuOptionGroup,
+  Box,
 } from '@chakra-ui/react';
 import { LanguageIcon, ChevronDownTrimmedIcon, ChevronUpTrimmedIcon } from '@contentful/f36-icons';
 import { useRouter } from 'next/router';
@@ -22,52 +23,59 @@ export const LanguageSelector = () => {
   const router = useRouter();
 
   return locales && locales.length > 1 ? (
-    <Flex justifyContent="center" alignItems="center">
-      <LanguageIcon width="18px" height="18px" variant="secondary" />
-      <Menu>
-        {({ isOpen }) => (
-          <>
-            <MenuButton
-              pr={1}
-              pl={1}
-              background="transparent"
-              _hover={{ bg: 'transparent' }}
-              _expanded={{ bg: 'transparent' }}
-              _focus={{ bg: 'transparent' }}
-              isActive={isOpen}
-              as={Button}
-              rightIcon={
-                isOpen ? (
-                  <ChevronUpTrimmedIcon variant="secondary" />
-                ) : (
-                  <ChevronDownTrimmedIcon variant="secondary" />
-                )
-              }>
-              {displayName(locale).of(localeName(locale))}
-            </MenuButton>
-            <MenuList minW="100px" mt="-12px">
-              <MenuOptionGroup
-                defaultValue={locale}
-                onChange={value => {
-                  router.push({ pathname: router.pathname, query: router.query }, router.asPath, {
-                    locale: String(value),
-                  });
-                }}>
-                {locales?.map(availableLocale => (
-                  <MenuItemOption
-                    p={0}
-                    _hover={{ bg: 'transparent' }}
-                    key={availableLocale}
-                    value={availableLocale}
-                    hidden={availableLocale === locale}>
-                    {displayName(availableLocale).of(localeName(availableLocale))}
-                  </MenuItemOption>
-                ))}
-              </MenuOptionGroup>
-            </MenuList>
-          </>
-        )}
-      </Menu>
-    </Flex>
+    <>
+      <Flex justifyContent="center" alignItems="center" display={{ base: 'none', lg: 'flex' }}>
+        <LanguageIcon width="18px" height="18px" variant="secondary" />
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton
+                pr={1}
+                pl={1}
+                textTransform="uppercase"
+                background="transparent"
+                _hover={{ bg: 'transparent' }}
+                _expanded={{ bg: 'transparent' }}
+                _focus={{ bg: 'transparent' }}
+                isActive={isOpen}
+                as={Button}
+                rightIcon={
+                  isOpen ? (
+                    <ChevronUpTrimmedIcon variant="secondary" />
+                  ) : (
+                    <ChevronDownTrimmedIcon variant="secondary" />
+                  )
+                }>
+                {localeName(locale)}
+              </MenuButton>
+              <MenuList minW="100px" mt="-12px">
+                <MenuOptionGroup
+                  defaultValue={locale}
+                  onChange={value => {
+                    router.push({ pathname: router.pathname, query: router.query }, router.asPath, {
+                      locale: String(value),
+                    });
+                  }}>
+                  {locales?.map(availableLocale => (
+                    <MenuItemOption
+                      p={0}
+                      _focus={{ bg: 'transparent' }}
+                      _hover={{ bg: 'transparent' }}
+                      key={availableLocale}
+                      value={availableLocale}
+                      hidden={availableLocale === locale}>
+                      {displayName(availableLocale).of(localeName(availableLocale))}
+                    </MenuItemOption>
+                  ))}
+                </MenuOptionGroup>
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      </Flex>
+      <Box display={{ base: 'block', lg: 'none' }}>
+        <LanguageIcon width="18px" height="18px" variant="secondary" />
+      </Box>
+    </>
   ) : null;
 };
