c6d27e8fd575bd44829f0160620a6cf716c0d152
Matt van Voorst <mattvanvoorst@contentful.com>
2022-12-01 12:15:51 +0100

    feat: [PLATO-0000] hero banner calculation fix - Fixed some tutorial code

--- src/components/features/hero-banner/HeroBanner.tsx
+++ src/components/features/hero-banner/HeroBanner.tsx
@@ -17,13 +17,13 @@ const StyledBox = styled(Box)`
 `;
 
 export const HeroBanner = ({
+  // Tutorial: contentful-and-the-starter-template.md
+  // Uncomment the line below to make the Greeting field available to render
+  // greeting,
   heroBannerHeadline,
   heroBannerHeadlineColor,
   heroBannerImage,
-}: // Tutorial: contentful-and-the-starter-template.md
-// Uncomment the line below to make the Greeting field available to render
-// greeting,
-PageLandingFieldsFragment) => {
+}: PageLandingFieldsFragment) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const headingRef = useRef<HTMLHeadingElement | null>(null);
   const [headingVisible, setHeadingVisible] = useState(false);
@@ -65,7 +65,7 @@ PageLandingFieldsFragment) => {
 
     window.addEventListener('resize', handleFontSize);
     return () => window.removeEventListener('resize', handleFontSize);
-  }, [headingVisible]);
+  }, []);
 
   return (
     <Grid position="relative" gridRow={2} gridColumn={1} mt={`-${HEADER_HEIGHT}px`}>
