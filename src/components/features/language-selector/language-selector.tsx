c75689da459f51f5fea8ca3a75fe494da6725710
Nkoyo Ating <atingenkay@gmail.com>
2022-12-06 15:17:09 +0100

    PLATO-323: add language selector for mobile

--- src/components/features/language-selector/language-selector.tsx
+++ src/components/features/language-selector/language-selector.tsx
@@ -6,10 +6,20 @@ import {
   MenuItemOption,
   Button,
   MenuOptionGroup,
+  Text,
   Box,
+  Select,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { LanguageIcon, ChevronDownTrimmedIcon, ChevronUpTrimmedIcon } from '@contentful/f36-icons';
 import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 const localeName = locale => locale.split('-')[0];
 
@@ -21,6 +31,8 @@ const displayName = locale =>
 export const LanguageSelector = () => {
   const { locale, locales } = useRouter();
   const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const firstField = useRef<HTMLSelectElement | null>(null);
 
   return locales && locales.length > 1 ? (
     <>
@@ -80,7 +92,37 @@ export const LanguageSelector = () => {
         </Menu>
       </Flex>
       <Box display={{ base: 'block', md: 'none', lg: 'none' }}>
-        <LanguageIcon width="18px" height="18px" variant="secondary" />
+        <LanguageIcon width="18px" height="18px" variant="secondary" onClick={onOpen} />
+        <Drawer isOpen={isOpen} onClose={onClose} placement="right" initialFocusRef={firstField}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader fontSize={20}>Regional Settings</DrawerHeader>
+
+            <DrawerBody>
+              <Text fontSize="md" fontWeight={600}>
+                Language
+              </Text>
+              <Select
+                size="md"
+                mt={2}
+                ref={firstField}
+                defaultValue={locale}
+                onChange={event => {
+                  router.push({ pathname: router.pathname, query: router.query }, router.asPath, {
+                    locale: String(event.target.value),
+                  });
+                  onClose();
+                }}>
+                {locales?.map(availableLocale => (
+                  <option key={availableLocale} value={availableLocale}>
+                    {displayName(availableLocale).of(localeName(availableLocale))}
+                  </option>
+                ))}
+              </Select>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
       </Box>
     </>
   ) : null;
