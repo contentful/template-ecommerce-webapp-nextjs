aad8bf62265f39aebbeccef81140586e3f19ab8c
Matt van Voorst <mattvanvoorst@contentful.com>
2023-05-03 16:35:35 +0200

    feat: [] Next.js preview mode Added support for Next.js their preview mode. https://nextjs.org/docs/advanced-features/preview-mode. API routes have been added to handle previews URLs, and to reset the preview mode.
    A `previewGraphQlClient` was added which is used in favor of the `graphQlClient` when the preview mode is active. The `index` and `slug` page have been modified accordingly to account for this.
    
    The node version was bumped as part of this PR

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'next-i18next';
 import { HeroBanner } from '@src/components/features/hero-banner';
 import { ProductTileGrid } from '@src/components/features/product';
 import { SeoFields } from '@src/components/features/seo';
-import { client } from '@src/lib/client';
+import { client, previewClient } from '@src/lib/client';
 import { getServerSideTranslations } from '@src/pages/utils/get-serverside-translations';
 
 const Page = ({ page }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -32,9 +32,11 @@ const Page = ({ page }: InferGetServerSidePropsType<typeof getServerSideProps>) 
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale, preview }) => {
   try {
-    const data = await client.pageLanding({ locale });
+    const gqlClient = preview ? previewClient : client;
+
+    const data = await gqlClient.pageLanding({ locale, preview });
 
     const page = data.pageLandingCollection?.items[0];
 
