009cbb3e128963a60064091e03dc4e699913203e
roosterhack <eddie.chung@contentful.com>
2023-08-11 15:08:26 +0200

    fix: [PLATO-842] Add domains to CSP and image optimaization

--- next.config.js
+++ next.config.js
@@ -58,7 +58,12 @@ const moduleExports = withPlugins(plugins, {
    * Settings are the defaults
    */
   images: {
-    domains: ['images.ctfassets.net', 'images.flinkly.com', 'images.quirely.com'],
+    domains: [
+      'images.ctfassets.net',
+      'images.eu.ctfassets.net',
+      'images.flinkly.com',
+      'images.quirely.com',
+    ],
   },
 
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
