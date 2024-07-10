81dd5effecff683ee67490c74865fb2299d0e525
Kathrin <kathrin.holzmann@contentful.com>
2024-07-10 16:40:07 +0200

    feat: input field without label

--- src/components/shared/quantity-selector/QuantitySelector.tsx
+++ src/components/shared/quantity-selector/QuantitySelector.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/aria-props */
 import { Input } from '@chakra-ui/input';
-import { Box, Button, Flex, FormControl, FormLabel, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, FormControl, Text } from '@chakra-ui/react';
 import { ShoppingCartIcon } from '@contentful/f36-icons';
 import { useTranslation } from 'next-i18next';
 
@@ -9,7 +10,7 @@ export const QuantitySelector = () => {
   return (
     <FormControl>
       <Text
-        as={FormLabel}
+        aria-role="label"
         variant="small"
         fontWeight="600"
         letterSpacing="0.1rem"
